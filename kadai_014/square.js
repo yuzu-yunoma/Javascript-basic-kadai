e5979853741fd7b1e0237485bce866ff2679975d
yuzu-yunoma <yunoma.dev@gmail.com>
2025-11-25 00:21:18 +0900

    Add files via upload

---
+++ kadai_014/square.js
@@ -0,0 +1,11 @@
+// 数値を二乗する関数
+const calculateSquare = (number) => {
+  // 数値を二乗してその結果を戻り値として返す
+  return number * number;
+};
+
+// 関数を使用して10の二乗を計算
+const result = calculateSquare(10);
+
+// 結果をコンソールに表示
+console.log(result); // 出力: 100
